5d3de7e7e8d431d54589de5e1be3c387002deac0
Aman Kumar <aman.kumar1_cs19@gla.ac.in>
2020-08-23 20:37:07 +0530

    add equals() and hashCode() method for book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,6 +6,8 @@
  * */
 package definitions;
 
+import java.util.Objects;
+
 public class Book {
     private String bookName;
     private String authorName;
@@ -44,5 +46,22 @@ public class Book {
     @Override
     public String toString() {
         return String.format("Book Name:%s,Author Name:%s,ISBN Number:%s", getBookName(), getAuthorName(), getIsbnNumber());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Book book = (Book) o;
+        return Objects.equals(bookName, book.bookName) &&
+                Objects.equals(authorName, book.authorName) &&
+                Objects.equals(isbnNumber, book.isbnNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, authorName, isbnNumber);
     }
 }
